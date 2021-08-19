170b2901dbcbd13d43f270a02473e8c3209e1cfa
Guillaume <66109302+TSEGGuillaume@users.noreply.github.com>
2021-08-19 22:55:33 +0200

    Block the notifications stacking in the Windows Tray.

--- SimpleScreenRestReminder.ps1
+++ SimpleScreenRestReminder.ps1
@@ -1,14 +1,18 @@
 ﻿Add-Type -AssemblyName  System.Windows.Forms 
 
+#TODO : 
+# - Find a way to restart the script after sleep mode
+
 function NotifyUserToRest
 {
     param (
         $message
     )
 
-    $path = (Get-Process -id $pid).Path
-
     $global:balloon = New-Object System.Windows.Forms.NotifyIcon
+    $notificationTime = 5000
+
+    $path = (Get-Process -id $pid).Path
 
     $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path) 
     $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info 
@@ -16,7 +20,10 @@ function NotifyUserToRest
     $balloon.BalloonTipTitle = "SimpleScreenRestReminder" 
     $balloon.Visible = $true 
 
-    $balloon.ShowBalloonTip(5000)
+    $balloon.ShowBalloonTip($notificationTime)
+
+    Start-Sleep -Milliseconds $notificationTime # Wait for the notification period
+    $balloon.Dispose() # Dispose of the notification in order to not stack it in the Windows tray
 }
 
 Write-Output "Start the eye rest reminder, following the 20-20-20 rule."
@@ -25,11 +32,13 @@ $baseTime = 20
 
 while ($true)
 {
-    # 20 minutes d'écran 
+    # 20 minutes of screen time
     Start-Sleep -s ($baseTime * 60)
+    Write-Output "It's time to watch the world beyond 20 meters."
     NotifyUserToRest -message "It's time to watch the world beyond 20 meters."
 
-    # 20 secondes de repos 
+    # 20 seconds of eye rest
     Start-Sleep -s ($baseTime * 1)
+    Write-Output "Let's back to work !"
     NotifyUserToRest -message "Let's back to work !"
 }
